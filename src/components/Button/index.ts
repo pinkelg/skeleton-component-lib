47272ad341e0967676172efdaabb218d4c28f0a5
Pinkel Gurung <pinkelgurung@gmail.com>
2022-11-01 23:06:33 -0400

    apply prettier

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,2 +1,2 @@
-export { default as Button} from "./Button";
-export { default as SuccessButton} from "./SuccessButton";
+export { default as Button } from "./Button";
+export { default as SuccessButton } from "./SuccessButton";
