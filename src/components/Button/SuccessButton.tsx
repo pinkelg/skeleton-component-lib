63fa97ac16d4752f389a4ddfa78cf289cb58ba0a
Pinkel Gurung <pinkelgurung@gmail.com>
2022-12-10 01:31:01 -0500

    clean project

--- src/components/Button/SuccessButton.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactElement } from "react";
-import Button, { ButtonProps } from "./Button";
-
-const SuccessButton = (props: ButtonProps): ReactElement<HTMLButtonElement> => {
-  return <Button {...props} label={"success"} />;
-};
-
-export default SuccessButton;
