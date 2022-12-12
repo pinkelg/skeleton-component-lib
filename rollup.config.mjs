09fc4d7ba231e5ae115a61acd2df3bed474392ef
pinkelg <97774000+pinkelg@users.noreply.github.com>
2022-12-11 19:11:59 -0500

    add rollup (#22)

---
+++ rollup.config.mjs
@@ -0,0 +1,68 @@
+import { DEFAULT_EXTENSIONS } from "@babel/core";
+import babel from "@rollup/plugin-babel";
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import resolve from "@rollup/plugin-node-resolve";
+import url from "@rollup/plugin-url";
+import svgr from "@svgr/rollup";
+import { terser } from "rollup-plugin-terser";
+import typescriptEngine from "typescript";
+import pkg from "./package.json" assert { type: "json" };
+
+const config = [
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: "esm",
+        exports: "named",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      postcss({
+        plugins: [],
+        minimize: true,
+      }),
+      external({
+        includeDependencies: true,
+      }),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        typescript: typescriptEngine,
+        include: ["*.js+(|x)", "**/*.js+(|x)"],
+        exclude: [
+          "coverage",
+          "config",
+          "dist",
+          "node_modules/**",
+          "*.test.{js+(|x), ts+(|x)}",
+          "**/*.test.{js+(|x), ts+(|x)}",
+        ],
+      }),
+      commonjs(),
+      babel({
+        extensions: [...DEFAULT_EXTENSIONS, ".ts", "tsx"],
+        babelHelpers: "runtime",
+        exclude: /node_modules/,
+      }),
+      url(),
+      svgr(),
+      resolve(),
+      terser(),
+    ],
+    watch: {
+      clearScreen: false,
+    },
+  },
+];
+export default config;
