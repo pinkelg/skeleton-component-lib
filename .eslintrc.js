0e93b6b9c2eaf7deba9ae01d6d76b9e9596a4042
pinkelg <97774000+pinkelg@users.noreply.github.com>
2022-12-17 21:25:28 -0500

    setup storybook (#25)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,26 @@
+const prettierConfig = require("./prettier.config");
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
     jest: true
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier", "plugin:storybook/recommended"],
   parser: "@typescript-eslint/parser",
   overrides: [],
   parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
     project: ["./tsconfig.json"]
   },
   settings: {
-    react: { version: "detect" },
+    react: {
+      version: "detect"
+    },
     "import/resolver": {
       node: {
         extensions: [".js", ".jsx", ".ts", ".tsx", ".d.ts"]
@@ -24,5 +28,16 @@ module.exports = {
     }
   },
   plugins: ["react", "@typescript-eslint", "react-hooks", "prettier"],
-  rules: {}
+  rules: {
+    "prettier/prettier": ["error", prettierConfig],
+    "import/no-anonymous-default-export": 0
+  },
+  "overrides": [
+    {
+      "files": ["**/*.stories.tsx"],
+      "rules": {
+        "@typescript-eslint/consistent-type-assertions": 0
+      }
+    }
+  ]
 };
---
+++ .storybook/main.js
@@ -0,0 +1,32 @@
+const custom = require("./webpack.config.js");
+
+module.exports = {
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/preset-scss",
+    "@storybook/preset-typescript",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-interactions",
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-docs",
+      options: { configureJSX: true }
+    },
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss")
+        }
+      }
+    }
+  ],
+  webpackFinal: (config) => {
+    return { ...config, resolve: { ...config.resolve, ...custom.resolve }, module: { ...config.module, rules: custom.module.rules } };
+  },
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+};
